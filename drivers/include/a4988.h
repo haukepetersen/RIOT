432bbe370af3078bbaa20878255f9ad60dfe4cd4
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-03-23 23:24:39 +0100

    asdf

--- drivers/include/a4988.h
+++ drivers/include/a4988.h
@@ -21,17 +21,23 @@ typedef struct {
 typedef enum {
     A4988_DIR_CW    = 0,
     A4988_DIR_CCW   = 1,
-    A4988_DIR_SHORT = 2
+    A4988_DIR_STOP  = 2
 } a4988_dir_t;
 
 
 int a4988_init(a4988_t *dev, gpio_t step, gpio_t dir, uint16_t steps_per_round);
 
-int a4988_step_fw(a4988_t *dev);
+int a4988_set_dir(a4988_t *dev, a4988_dir_t dir)
+
+int a4988_step(a4988_t *dev, a4988_dir_t dir);
+
+int a4988_step
+
+
 int a4988_step_bw(a4988_t *dev);
 
 int a4988_set_speed(a4988_t *dev, uint16_t 10thdeg_per_sec);
 
-int a4988_set_abs(a4988_t *dev, uint16_t pos, a4988_dir_t dir);
+int a4988_set_abs(a4988_t *dev, int16_t pos, a4988_dir_t dir);
 
-int a4988_set_rel(a4988_t *dev, uint16_t pos, a4988_dir_t dir);
+int a4988_set_rel(a4988_t *dev, int16_t pos, a4988_dir_t dir);
