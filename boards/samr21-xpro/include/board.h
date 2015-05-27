5ab7dc84d5e49c45864e447f5bb4a697082410f7
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-05-27 18:03:52 +0200

    boards/samr21: adapted to new GPIO driver

--- boards/samr21-xpro/include/board.h
+++ boards/samr21-xpro/include/board.h
@@ -22,6 +22,7 @@
 #define __BOARD_H
 
 #include "cpu.h"
+#include "periph_cpu.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -42,10 +43,10 @@ extern "C" {
 * @{
 */
 #define NG_AT86RF233_SPI        (SPI_0)
-#define NG_AT86RF233_CS         (GPIO_4)
-#define NG_AT86RF233_INT        (GPIO_5)
-#define NG_AT86RF233_RESET      (GPIO_6)
-#define NG_AT86RF233_SLEEP      (GPIO_7)
+#define NG_AT86RF233_CS         GPIO(PORT_B, 31)
+#define NG_AT86RF233_INT        GPIO(PORT_B, 0)
+#define NG_AT86RF233_RESET      GPIO(PORT_B, 15)
+#define NG_AT86RF233_SLEEP      GPIO(PORT_A, 20)
 #define NG_AT86RF233_SPI_CLK    (SPI_SPEED_1MHZ)
 /** @}*/
 
@@ -54,10 +55,10 @@ extern "C" {
 * @{
 */
 #define AT86RF231_SPI      SPI_0
-#define AT86RF231_CS       GPIO_4
-#define AT86RF231_INT      GPIO_5
-#define AT86RF231_RESET    GPIO_6
-#define AT86RF231_SLEEP    GPIO_7
+#define AT86RF231_CS       GPIO(PORT_B, 31)
+#define AT86RF231_INT      GPIO(PORT_B, 0)
+#define AT86RF231_RESET    GPIO(PORT_B, 15)
+#define AT86RF231_SLEEP    GPIO(PORT_A, 20)
 
 #define AT86RF231_SPI_SPEED SPI_SPEED_1MHZ
 /** @} */
