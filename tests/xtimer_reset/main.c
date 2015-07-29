00763414f8520aca2a5b520cb38a164d5c3f981a
Kaspar Schleiser <kaspar@schleiser.de>
2015-07-29 15:18:00 +0200

    squash: tests wtimer_reset -> xtimer_reset

--- tests/xtimer_reset/main.c
+++ tests/xtimer_reset/main.c
@@ -11,7 +11,7 @@
  * @{
  *
  * @file
- * @brief       wtimer re-set test application
+ * @brief       xtimer re-set test application
  *
  * @author      Kaspar Schleiser <kaspar@schleiser.de>
  *
@@ -21,36 +21,36 @@
 #include <stdio.h>
 
 #include "thread.h"
-#include "wtimer.h"
-extern void wtimer_dump_all(void);
+#include "xtimer.h"
+extern void xtimer_dump_all(void);
 int main(void)
 {
     printf("This test tests re-setting of an already active timer.\n");
     printf("It should print three times \"now=<value>\", with values"
            " approximatly 100ms (100000us) apart.\n");
 
-    wtimer_t wtimer;
-    wtimer_t wtimer2;
+    xtimer_t xtimer;
+    xtimer_t xtimer2;
 
     kernel_pid_t me = thread_getpid();
 
-    wtimer_set_wakeup(&wtimer, 1<<31, me);
-    wtimer_set_wakeup(&wtimer2, 1<<31, me);
+    xtimer_set_wakeup(&xtimer, 1<<31, me);
+    xtimer_set_wakeup(&xtimer2, 1<<31, me);
 
-    wtimer_set_wakeup(&wtimer, 1<<16, me);
-    wtimer_set_wakeup(&wtimer2, 1<<16, me);
+    xtimer_set_wakeup(&xtimer, 1<<16, me);
+    xtimer_set_wakeup(&xtimer2, 1<<16, me);
 
-    wtimer_set_wakeup(&wtimer, 4000000, me);
-    wtimer_set_wakeup(&wtimer2, 3000000, me);
+    xtimer_set_wakeup(&xtimer, 4000000, me);
+    xtimer_set_wakeup(&xtimer2, 3000000, me);
 
-    wtimer_set_wakeup(&wtimer, 200000, me);
-    wtimer_set_wakeup(&wtimer2, 100000, me);
+    xtimer_set_wakeup(&xtimer, 200000, me);
+    xtimer_set_wakeup(&xtimer2, 100000, me);
 
-    printf("now=%u\n", (unsigned)wtimer_now());
+    printf("now=%u\n", (unsigned)xtimer_now());
     thread_sleep();
-    printf("now=%u\n", (unsigned)wtimer_now());
+    printf("now=%u\n", (unsigned)xtimer_now());
     thread_sleep();
-    printf("now=%u\n", (unsigned)wtimer_now());
+    printf("now=%u\n", (unsigned)xtimer_now());
 
     printf("Test completed!\n");
 
