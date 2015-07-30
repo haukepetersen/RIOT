a1cedf00c86342d10cebbfc2763ab4e0af68826f
Kaspar Schleiser <kaspar@schleiser.de>
2015-07-30 12:48:14 +0200

    squash: WTIMER->XTIMER int cpu/native/include/periph_conf.h

--- cpu/native/include/periph_conf.h
+++ cpu/native/include/periph_conf.h
@@ -52,8 +52,8 @@
 #define HW_TIMER TIMER_0
 /** @} */
 
-#define WTIMER_OVERHEAD 14
-#define WTIMER_USLEEP_UNTIL_OVERHEAD 1
+#define XTIMER_OVERHEAD 14
+#define XTIMER_USLEEP_UNTIL_OVERHEAD 1
 
 #ifdef __cplusplus
 }
