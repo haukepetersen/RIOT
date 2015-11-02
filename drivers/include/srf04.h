4c10fadbf1690cf17229ae38657666a9c6490fe4
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-11-02 14:57:54 +0100

    asdf

--- drivers/include/srf04.h
+++ drivers/include/srf04.h
@@ -1,37 +1,26 @@
 
+#include <stdint.h>
 
+#include "mutex.h"
 #include "periph/gpio.h"
-#include "periph/incap.h"
 
-#define SRF04_MSG_DATA      (0x01)
 
 typedef struct {
     gpio_t trigger;
-    incap_t pulse;
+    gpio_t echo;
+    uint32_t pulse_len;
+    mutex_t lock;
 } srf04_t;
 
 
-int srf04_init_auto(void);
+typedef enum {
+    SRF04_MODE_INCH,
+    SRF04_MODE_MM,
+    SRF04_MODE_MS
+} srf04_mode_t;
 
 
-int srf04_init(srf04_t *dev, gpio_t trigger, incap_t pulse);
 
+int srf04_init(srf04_t *dev, gpio_t trigger_pin, gpio_t echo_pin);
 
-int srf04_measure_once(srf04_t *dev);
-
-int srf04_measure(srf04_t *dev);
-
-int srf04_stop(srf04_t *dev);
-
-/**
- * @brief Sample a single distance value from the given sensor
- * 
- * This function will block until the value is ready, which will take
- * from 
- * 
- * @param[in] dev       the device to read from
- * 
- * @return              the distance value in mm
- * @return              -1 on error
- */
-int srf04_sample(srf04_t dev);
+uint16_t srf04_get_distance(srf04_t *dev, srf04_mode_t mode);
--- drivers/srf04/srf04.c
+++ drivers/srf04/srf04.c
@@ -3,12 +3,77 @@
 
 #include "srf04.h"
 
+#define SRF04_MSG_DATA      (0x01)
 
 #define SPEED_OF_SOUND      (340290U)       /* speed of sound in mm per second */
 
-void pulse_cb(int pw);
+/**
+ * @brief   The datasheet tells us, that the trigger pulse should be 10us wide
+ */
+#define TRIGGER_DELAY       (10U)
 
-void trigger(srf04_t *dev);
+
+#define NOTHING_IN_RANGE    (32000U)
+
+static void echo_cb(void *arg)
+{
+    srf04_t *dev = (srf04_t *)arg;
+
+    uint32_t now = xtimer_now();
+
+    if (gpio_get(dev->echo)) {
+        dev->pulse = now;
+    }
+    else {
+        if (now > dev->pulse) {
+            dev->pulse = now - dev->pulse;
+        }
+        else {
+            dev->pulse = now + (0xffffffff - dev->pulse);
+        }
+        mutex_unlock(&dev->lock);
+    }
+}
+
+int srf04_init(srf04_t *dev, gpio_t trigger_pin, gpio_t echo_pin)
+{
+    /* remember pins and initialize mutex */
+    dev->trigger = trigger_pin;
+    dev->echo = echo_pin;
+    mutex_init(&dev->lock);
+    mutex_lock(&dev->lock);
+
+    /* initialize pins */
+    gpio_init(dev->trigger, GPIO_DIR_OUT, GPIO_NOPULL);
+    gpio_clear(dev->trigger);
+    gpio_init_int(dev->echo, GPIO_FLANK_BOTH, GPIO_NOPULL, echo_cb, dev);
+
+    return 0;
+}
+
+uint16_t srf04_get_distance(srf04_t *dev, srf04_mode_t mode)
+{
+    uint32_t res;
+
+    /* send trigger signal */
+    gpio_set(dev->trigger);
+    xtimer_usleep(TRIGGER_DELAY);
+    gpio_clear(dev->trigger);
+
+    /* wait for echo pulse */
+    mutex_lock(&dev->lock);
+    /* check if anything was in range */
+    if (dev->pulse >= NOTHING_IN_RANGE) {
+        return 0;
+    }
+    /* compute actual distance value */
+    switch (mode) {
+        case SRF04_MODE_INCH:
+
+    }
+}
+
+
 
 
 #if SRF04_TRIGGER && SRF04_PULSE && SRF04_DEV
@@ -17,6 +82,8 @@ int srf04_init_auto(void)
 
 }
 #endif
+
+
 
 
 int srf04_init(srf04_t *dev, gpio_t trigger, incap_t pulse)
@@ -53,7 +120,7 @@ void pulse_cb(int pw)
     /* pw holds the response pulse width in us.
        To extract the distance in mm, d=pw/1000/s_sound/2 */
     uint32_t distance = pw / 2 / 1000000 * SPEED_OF_SOUND;
-    
+
     /* send distance to interested thread */
     msg.type = SRF04_MSG_DATA
     msg.value = distance;
