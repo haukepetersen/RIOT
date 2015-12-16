6639aa354a7a3cc604361aaba3708f0e32e4d98c
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-12-16 13:05:06 +0100

    mia: started on CoAP

---
+++ sys/include/net/mia/coap.h
@@ -0,0 +1,28 @@
+
+
+
+#ifndef MIA_COAP_H
+#define MIA_COAP_H
+
+#include "net/mia.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void(*mia_coap_cb)(void);
+
+typedef struct {
+    uint32_t ep;
+    mia_cb_t get;
+    mia_cb_t post;
+} mia_coap_ep_t
+
+void mia_coap_process(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MIA_COAP_H */
---
+++ sys/net/mia/mia_coap.c
@@ -0,0 +1,13 @@
+
+
+
+#include "net/mia/coap.h"
+
+
+extern mia_coap_ep_t mia_coap_eps[];
+
+
+void mia_coap_process(void)
+{
+
+}
