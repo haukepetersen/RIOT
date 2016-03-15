8bc9990db10d8dc949b57e92700a6328a0665438
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-03-15 16:08:59 +0100

    s driver/spi: some improvements
    - fixed style issues
    - fixed default type values
    - made single byte transfer functions return the receive byte

--- drivers/include/periph/spi.h
+++ drivers/include/periph/spi.h
@@ -29,8 +29,6 @@
 #include "periph_cpu.h"
 #include "periph_conf.h"
 #include "periph/gpio.h"
-/* TODO: remove once all platforms are ported to this interface */
-#include "periph/dev_enums.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -47,14 +45,18 @@ extern "C" {
  * @brief   Define global value for undefined SPI device
  */
 #ifndef SPI_UNDEF
-#define SPI_UNDEF       (-1)
+#define SPI_UNDEF       (UINT_MAX)
 #endif
 
 /**
  * @brief   Default SPI chip select pin access macro
+ *
+ * UINT_MAX is the default value for GPIO_UNDEF, so we go one less here. This
+ * still gives us the highest possible chance not to collide with and valid
+ * GPIO_PIN(x,y) value.
  */
 #ifndef SPI_HWCS
-#define SPI_HWCS(x)     (INT_MIN + x)
+#define SPI_HWCS(x)     (UINT_MAX - 1 - x)
 #endif
 
 /**
@@ -97,10 +99,10 @@ typedef gpio_t spi_cs_t;
  */
 #ifndef HAVE_SPI_MODE_T
 typedef enum {
-    SPI_MODE_0,                 /**< CPOL=0, CPHA=0 */
-    SPI_MODE_1,                 /**< CPOL=0, CPHA=1 */
-    SPI_MODE_2,                 /**< CPOL=1, CPHA=0 */
-    SPI_MODE_3                  /**< CPOL=1, CPHA=1 */
+    SPI_MODE_0,             /**< CPOL=0, CPHA=0 */
+    SPI_MODE_1,             /**< CPOL=0, CPHA=1 */
+    SPI_MODE_2,             /**< CPOL=1, CPHA=0 */
+    SPI_MODE_3              /**< CPOL=1, CPHA=1 */
 } spi_mode_t;
 #endif
 
@@ -113,11 +115,11 @@ typedef enum {
  */
 #ifndef HAVE_SPI_CLK_T
 typedef enum {
-    SPI_CLK_100KHZ = 0,         /**< drive the SPI bus with 100KHz */
-    SPI_CLK_400KHZ,             /**< drive the SPI bus with 400KHz */
-    SPI_CLK_1MHZ,               /**< drive the SPI bus with 1MHz */
-    SPI_CLK_5MHZ,               /**< drive the SPI bus with 5MHz */
-    SPI_CLK_10MHZ               /**< drive the SPI bus with 10MHz */
+    SPI_CLK_100KHZ,         /**< drive the SPI bus with 100KHz */
+    SPI_CLK_400KHZ,         /**< drive the SPI bus with 400KHz */
+    SPI_CLK_1MHZ,           /**< drive the SPI bus with 1MHz */
+    SPI_CLK_5MHZ,           /**< drive the SPI bus with 5MHz */
+    SPI_CLK_10MHZ           /**< drive the SPI bus with 10MHz */
 } spi_clk_t;
 #endif
 
@@ -126,15 +128,28 @@ typedef enum {
  *
  * @param[in] dev       initialize pins for this SPI device
  * @param[in] cs        also initialize this chip select pin
+ *
+ * @return              0 on success
+ * @return              -1 on invalid device
+ * @return              -2 on invalid CS
  */
-int spi_init_pins(spi_t dev, spi_cs_t cs);
+int spi_init(spi_t dev, spi_cs_t cs);
 
 /**
- * @brief Get mutually exclusive access to the given SPI bus
+ * @brief   Start a new SPI transaction
  *
- * In case the SPI device is busy, this function will block until the bus is free again.
+ * Starting a new SPI transaction will get exclusive access to the SPI bus
+ * and configure it according to the given values. If another SPI transaction
+ * is active when this function is called, this function will block until the
+ * other transaction is complete (spi_relase was called).
  *
  * @param[in] dev       SPI device to access
+ * @param[in] mode      mode to use for the new transaction
+ * @param[in] clk       bus clock speed to use for the transaction
+ * @param[in] cs        chip select pin to use
+ *
+ * @return              0 on success
+ * @return              -1 on error
  */
 int spi_acquire(spi_t dev, spi_mode_t mode, spi_clk_t clk, spi_cs_t cs);
 
@@ -150,10 +165,10 @@ void spi_release(spi_t dev);
  *
  * @param[in] dev       SPI device to use
  * @param[in] out       Byte to send out, set NULL if only receiving
- * @param[out] in       Byte to read, set NULL if only sending
+ *
+ * @return              The byte that was received
  */
-void spi_transfer_byte(spi_t bus, spi_cs_t cs, bool cont,
-                       uint8_t out, uint8_t *in);
+uint8_t spi_transfer_byte(spi_t bus, spi_cs_t cs, bool cont, uint8_t out);
 
 /**
  * @brief Transfer a number bytes on the given SPI bus
@@ -178,11 +193,9 @@ void spi_transfer_bytes(spi_t bus, spi_cs_t cs, bool cont,
  * @param[in] out       Byte to send, set NULL if only receiving data
  * @param[out] in       Byte to read, set NULL if only sending
  *
- * @return              Number of bytes that were transfered
- * @return              -1 on error
+ * @return              The value that was read from the given register address
  */
-void spi_transfer_reg(spi_t bus, spi_cs_t cs,
-                      uint8_t reg, uint8_t out, uint8_t *in);
+uint8_t spi_transfer_reg(spi_t bus, spi_cs_t cs, uint8_t reg, uint8_t out);
 
 /**
  * @brief Transfer a number of bytes from/to a given register address
@@ -196,9 +209,6 @@ void spi_transfer_reg(spi_t bus, spi_cs_t cs,
  * @param[in] out       Byte array to send data from, set NULL if only receiving
  * @param[out] in       Byte buffer to read into, set NULL if only sending
  * @param[in] length    Number of bytes to transfer
- *
- * @return              Number of bytes that were transfered
- * @return              -1 on error
  */
 void spi_transfer_regs(spi_t bus, spi_cs_t cs,
                        uint8_t reg, uint8_t *out, uint8_t *in, size_t len);
