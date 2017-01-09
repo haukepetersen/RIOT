3c30bfc15d69944bd1494acc8d7e311b68fce3f0
Kaspar Schleiser <kaspar@schleiser.de>
2017-01-09 22:55:53 +0100

    cpu: k60: add periph/pm support

---
+++ cpu/k60/include/periph_cpu.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup         cpu_k60
+ * @{
+ *
+ * @file
+ * @brief           CPU specific peripheral configuration options
+ *
+ * @author          Kaspar Schleiser <kaspar@schleiser.de>
+ */
+
+#ifndef PERIPH_CPU_H
+#define PERIPH_CPU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PM_NUM_MODES    (1U)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CPU_H */
+/** @} */
