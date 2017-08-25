6bf31e03524261b04252ad424391f0cab5008d9c
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-08-25 09:13:23 +0200

    drivers: add soft uart driver

---
+++ drivers/include/soft_uart.h
@@ -0,0 +1,16 @@
+
+
+
+#ifndef SOFT_UART_H
+#define SOFT_UART_H
+
+#include "xtimer.h"
+#include "periph/uart2.h"
+
+typedef struct {
+    gpio_t pin_tx;
+    gpio_t pin_rx;
+
+}
+
+#endif /* SOFT_UART_H */
