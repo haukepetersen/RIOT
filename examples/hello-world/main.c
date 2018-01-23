6216d73b80f3fbc81a2870cf8a1b189edccf39d4
Hauke Petersen <hauke.petersen@fu-berlin.de>
2018-01-23 17:47:02 +0100

    TMP: debugging

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -21,12 +21,39 @@
 
 #include <stdio.h>
 
+#include "xtimer.h"
+#include "sx150x.h"
+#include "sx150x_params.h"
+
+static sx150x_t sx;
+
+#define PIN   (15U)
+
 int main(void)
 {
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+
+    sx150x_init(&sx, &sx150x_params[0]);
+
+    sx150x_gpio_init(&sx, PIN, GPIO_OUT);
+    // sx150x_gpio_init(&sx, 15, GPIO_OUT);
+
+
+    while (1) {
+        // sx150x_gpio_set(&sx, PIN);
+        // printf("pin %i on", (int)PIN);
+        xtimer_usleep(500 * 1000);
+
+        // sx150x_gpio_clear(&sx, PIN);
+        // printf("pin %i off", (int)PIN);
+        xtimer_usleep(500 * 1000);
+
+        // sx150x_gpio_toggle(&sx, 15);
+    }
 
     return 0;
 }
