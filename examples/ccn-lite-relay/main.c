8f3d9c59a93dd1d4007a5b429abb199207a01bd1
Martine Lenders <m.lenders@fu-berlin.de>
2018-02-27 12:26:52 +0100

    ccn-lite-relay: adapt for pktcnt

--- examples/ccn-lite-relay/main.c
+++ examples/ccn-lite-relay/main.c
@@ -20,24 +20,32 @@
 
 #include <stdio.h>
 
+#ifdef MODULE_TLSF
 #include "tlsf-malloc.h"
+#endif
 #include "msg.h"
 #include "shell.h"
 #include "ccn-lite-riot.h"
 #include "net/gnrc/netif.h"
 #include "net/gnrc/pktdump.h"
+#include "pktcnt.h"
 
 /* main thread's message queue */
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
+#ifdef MODULE_TLSF
 /* 10kB buffer for the heap should be enough for everyone */
 #define TLSF_BUFFER     (10240 / sizeof(uint32_t))
 static uint32_t _tlsf_heap[TLSF_BUFFER];
+#endif
 
 int main(void)
 {
+    uint16_t src_len = 8;
+#ifdef MODULE_TLSF
     tlsf_create_with_pool(_tlsf_heap, sizeof(_tlsf_heap));
+#endif
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
 
     puts("Basic CCN-Lite example");
@@ -47,11 +55,18 @@ int main(void)
     ccnl_start();
 
     /* get the default interface */
-    gnrc_netif_t *netif;
+    gnrc_netif_t *netif = gnrc_netif_iter(NULL);
+
+    gnrc_netapi_set(netif->pid, NETOPT_SRC_LEN, 0, &src_len, sizeof(src_len));
+
+    /* init pktcnt */
+    if (pktcnt_init() != PKTCNT_OK) {
+        puts("error: unable to initialize pktcnt");
+        return 1;
+    }
 
     /* set the relay's PID, configure the interface to use CCN nettype */
-    if (((netif = gnrc_netif_iter(NULL)) == NULL) ||
-        (ccnl_open_netif(netif->pid, GNRC_NETTYPE_CCN) < 0)) {
+    if (ccnl_open_netif(netif->pid, GNRC_NETTYPE_CCN) < 0) {
         puts("Error registering at network interface!");
         return -1;
     }
