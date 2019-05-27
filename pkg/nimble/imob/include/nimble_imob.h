bb1f474b1af5f4c97e07a61313ea14160b2d67e8
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-05-27 10:49:43 +0200

    pkg/nimble: add IPv6-mesh-over-BLE implementation

---
+++ pkg/nimble/imob/include/nimble_imob.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018-2019 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    pkg_nimble_imob IPv6 Mesh over BLE
+ * @ingroup     pkg_nimble
+ * @brief       This module provides GNRC netif support for NimBLE
+ *
+ * # High-level doc
+ * todo...
+ *
+ * @{
+ *
+ * @file
+ * @brief       GNRC netif integration for NimBLE
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef PKG_NIMBLE_IMOB_H
+#define PKG_NIMBLE_IMOB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PKG_NIMBLE_IMOB_H */
+/** @} */
