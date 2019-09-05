7ade8a381f0df3793a3649e0a1aec3373f86618f
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-09-05 14:54:07 +0200

    tests: add test application for nimble_rpble

---
+++ tests/nimble_gnrc_rpble/main.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Example for showing 6LoWPAN over BLE using NimBLE and GNRC
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "msg.h"
+#include "shell.h"
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+int main(void)
+{
+    puts("RPL-over-BLE Example Application");
+
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming networking packets (ping6) */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+
+    /* start shell */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
