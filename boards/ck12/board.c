444f0970ab02da1165da5912f57163ca06988570
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-09-16 17:30:57 +0200

    boards: add basic support for CK12 smart watch

---
+++ boards/ck12/board.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2019 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_ck12
+ * @{
+ *
+ * @file
+ * @brief       Board initialization for the CK12 smart watch
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+}
