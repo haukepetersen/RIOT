42be206b9258fe2e9ca85a43de2d977b13abe3ca
Hauke Petersen <hauke.petersen@fu-berlin.de>
2020-04-01 12:50:07 +0200

    expstats: add ND

--- sys/net/gnrc/network_layer/ndp/gnrc_ndp.c
+++ sys/net/gnrc/network_layer/ndp/gnrc_ndp.c
@@ -29,6 +29,16 @@
 
 #include "timex.h"
 
+#ifdef MODULE_EXPSTATS
+#include "expstats.h"
+#define EXPSTAT(x)      expstats_log(x)
+#else
+#define EXPSTAT(x)      (void)
+#endif
+
+#define ENABLE_DEBUG    (0)
+#include "debug.h"
+
 #define ENABLE_DEBUG 0
 #include "debug.h"
 
@@ -305,6 +315,7 @@ void gnrc_ndp_nbr_sol_send(const ipv6_addr_t *tgt, gnrc_netif_t *netif,
                 DEBUG("ndp: unable to send neighbor solicitation\n");
                 break;
             }
+            EXPSTAT(EXPSATAS_ND_NBR_SOL);
         }
         gnrc_netif_release(netif);
         return;
@@ -394,6 +405,7 @@ void gnrc_ndp_nbr_adv_send(const ipv6_addr_t *tgt, gnrc_netif_t *netif,
                 DEBUG("ndp: unable to send neighbor advertisement\n");
                 break;
             }
+            EXPSTAT(EXPSTATS_ND_NBR_ADV);
         }
         gnrc_netif_release(netif);
         return;
@@ -451,6 +463,7 @@ void gnrc_ndp_rtr_sol_send(gnrc_netif_t *netif, const ipv6_addr_t *dst)
                 DEBUG("ndp: unable to send router advertisement\n");
                 break;
             }
+            EXPSTAT(EXPSTATS_ND_RTR_SOL);
         }
         gnrc_netif_release(netif);
         return;
@@ -564,6 +577,7 @@ void gnrc_ndp_rtr_adv_send(gnrc_netif_t *netif, const ipv6_addr_t *src,
                 DEBUG("ndp: unable to send router solicitation\n");
                 break;
             }
+            EXPSTAT(EXPSTATS_ND_RTR_ADV);
         }
         gnrc_netif_release(netif);
         return;
