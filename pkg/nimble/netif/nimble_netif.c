ea3074ed56d692b9b7926afdf580fcb47becc87f
Hauke Petersen <hauke.petersen@fu-berlin.de>
2020-06-05 10:05:52 +0200

    seqstats: nimble_netif: remove lock counter

--- pkg/nimble/netif/nimble_netif.c
+++ pkg/nimble/netif/nimble_netif.c
@@ -150,9 +150,6 @@ static int _send_pkt(nimble_netif_conn_t *conn, gnrc_pktsnip_t *pkt)
 #ifdef MODULE_EXPSTATS
             expstats_log(EXPSTATS_NETIF_TX_PKT_BUSY);
 #endif
-#ifdef MODULE_SEQSTATS
-            seqstats_inc(SEQSTATS_NETIF_LOCK);
-#endif
             thread_flags_wait_all(FLAG_TX_UNSTALLED);
         }
     } while (res == BLE_HS_EBUSY);
@@ -440,9 +437,6 @@ static int _on_l2cap_client_evt(struct ble_l2cap_event *event, void *arg)
             _on_data(conn, event);
             break;
         case BLE_L2CAP_EVENT_COC_TX_UNSTALLED:
-#ifdef MODULE_SEQSTATS
-            seqstats_inc(SEQSTATS_NETIF_UNLOCK);
-#endif
             thread_flags_set(_netif_thread, FLAG_TX_UNSTALLED);
             break;
         default:
@@ -493,9 +487,6 @@ static int _on_l2cap_server_evt(struct ble_l2cap_event *event, void *arg)
             _on_data(conn, event);
             break;
         case BLE_L2CAP_EVENT_COC_TX_UNSTALLED:
-#ifdef MODULE_SEQSTATS
-            seqstats_inc(SEQSTATS_NETIF_UNLOCK);
-#endif
             thread_flags_set(_netif_thread, FLAG_TX_UNSTALLED);
             break;
         default:
