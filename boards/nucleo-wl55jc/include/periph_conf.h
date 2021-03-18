afb135b25e8a881d029052c433dbb9621f8e04b0
Hauke Petersen <hauke.petersen@fu-berlin.de>
2021-03-18 11:57:23 +0100

    board: fix UART configuration

--- boards/nucleo-wl55jc/include/periph_conf.h
+++ boards/nucleo-wl55jc/include/periph_conf.h
@@ -66,26 +66,33 @@ static const timer_conf_t timer_config[] = {
  */
 static const uart_conf_t uart_config[] = {
     {
-        .dev        = USART1,
-        .rcc_mask   = RCC_APB2ENR_USART1EN,
+        .dev        = LPUART1,
+        .rcc_mask   = RCC_APB1ENR2_LPUART1EN,
         .rx_pin     = GPIO_PIN(PORT_A, 3),
         .tx_pin     = GPIO_PIN(PORT_A, 2),
+        .rx_af      = GPIO_AF8,
+        .tx_af      = GPIO_AF8,
+        .bus        = APB12,
+        .irqn       = LPUART1_IRQn,
+        .type       = STM32_LPUART,
+        .clk_src    = 0, /* Use APB clock */
+    },
+    {
+        .dev        = USART1,
+        .rcc_mask   = RCC_APB2ENR_USART1EN,
+        .rx_pin     = GPIO_PIN(PORT_B, 7),
+        .tx_pin     = GPIO_PIN(PORT_B, 6),
         .rx_af      = GPIO_AF7,
         .tx_af      = GPIO_AF7,
         .bus        = APB2,
         .irqn       = USART1_IRQn,
-        // .type       = STM32_USART,
-        // .clk_src    = 0, /* Use APB clock */
-#ifdef MODULE_PERIPH_DMA
-        .dma        = 3,
-        .dma_chan   = 2
-#endif
+        .type       = STM32_USART,
+        .clk_src    = 0, /* Use APB clock */
     },
 };
 
-#define UART_0_ISR          (isr_usart2)
-#define UART_1_ISR          (isr_usart3)
-#define UART_2_ISR          (isr_usart1)
+#define UART_0_ISR          (isr_lpuart1)
+#define UART_1_ISR          (isr_usart1)
 
 #define UART_NUMOF          ARRAY_SIZE(uart_config)
 /** @} */
